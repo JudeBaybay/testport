88c4542114cdd21e38dc0e302e044bb01de18b1d
unknown <judeanzelmo@gmail.com>
2025-06-06 19:20:43 +0800

    new

--- assests/js/main.js
+++ assests/js/main.js
@@ -19,12 +19,6 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-function scrollHeader() {
-  const header = document.getElementById("header");
-  if (this.scrollY >= 50) header.classList.add("scroll-header");
-  else header.classList.add("scroll-header");
-}
-window.addEventListener("scroll", scrollHeader);
 
 var mixerProjects = mixitup(".projects__container", {
   selectors: {
